2a66dd7485273368a67f99ea3f3fff4a56972f53
leoliu <leoliu@yunify.com>
2020-12-14 11:04:09 +0800

    fix: Fix import cluster function error
    Signed-off-by: leoliu <leoliu@yunify.com>

--- src/actions/cluster.js
+++ src/actions/cluster.js
@@ -76,7 +76,7 @@ const handleImport = async (store, data) => {
     await store.validate(postData)
   }
 
-  await store.import(postData)
+  await store.create(postData)
 }
 
 const handleCreate = (store, data) => {
